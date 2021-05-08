f524707300bc755e0af3e3d337a9b269a8c9cc61
canislatranscoxus <canislatranscoxus@gmail.com>
2021-05-07 23:39:32 -0500

    config

---
+++ load_config/load_config.py
@@ -0,0 +1,27 @@
+'''
+Description: Example of loading a config file
+
+
+references: https://docs.python.org/3/library/configparser.html
+'''
+import configparser
+config = configparser.ConfigParser()
+
+
+file_path = '/home/art/git/poc/load_config/config.ini'
+config.read( file_path )
+
+print( config.sections() )
+# ['bitbucket.org', 'topsecret.server.com']
+
+print(  'bitbucket.org' in config )
+#True
+
+print( 'bytebong.com' in config )
+#False
+
+print( config['bitbucket.org']['User'] )
+#'hg'
+
+print( config['DEFAULT']['Compression'] )
+# yes
