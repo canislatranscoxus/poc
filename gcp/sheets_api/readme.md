5af451c0635030553e434c20f09671b9a0ac2c2e
canislatranscoxus <canislatranscoxus@gmail.com>
2023-06-17 18:47:39 -0500

    google sheets

---
+++ gcp/sheets_api/readme.md
@@ -0,0 +1,24 @@
+# Gatojazz Dictionary - Google Sheet
+
+In this mini project, we will access a google sheet from a python script.
+
+
+Remember to install google api python client.
+
+```
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+
+## How to run the script
+
+```
+python3 quickstart.py
+```
+
+
+
+## links
+
+* https://developers.google.com/sheets/api/quickstart/python
+
+
---
+++ gcp/sheets_api/sheet_01.py
@@ -0,0 +1,37 @@
+'''
+
+links:
+
+https://developers.google.com/sheets/api/guides/concepts
+https://developers.google.com/workspace/guides/enable-apis
+
+'''
+
+print( 'We will connect to Google Sheets' )
+
+url = 'https://docs.google.com/spreadsheets/d/
+1FuTG3zZP1dtjgCAHCAVBFC62HHEJaI_M8chbSkqI9x0/edit#gid=0'
+
+#user = input( 'user: ' )
+user = ''
+
+password = input( 'pass: ' )
+
+
+
+print( 'Connect' )
+
+
+print( 'read data' )
+print( 'worksheet: {}' )
+print( 'sheet    : {}' )
+print( 'cell     : {}' )
+
+
+print( 'write data' )
+print( 'worksheet: {}' )
+print( 'sheet    : {}' )
+print( 'cell     : {}' )
+
+
+print( 'end' )
